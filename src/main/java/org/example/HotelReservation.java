5f4596448ddd720e5ba9dbebb88cfbd1a2b39015
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-28 11:09:19 +0530

    Update HotelReservation.java

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -13,28 +13,4 @@ public class HotelReservation{
     public void addHotel(Hotel hotel){
         hotels.add(hotel);
     }
-
-    public String findCheapestHotel(LocalDate startDate, LocalDate endDate){
-        int min_cost = Integer.MAX_VALUE;
-        String cheapestHotel = null;
-
-        for(Hotel hotel: hotels){
-            int totalCost = calculateTotalHotelCost(hotel, startDate, endDate);
-            if(totalCost<min_cost){
-                min_cost = totalCost;
-                cheapestHotel = hotel.getName();
-            }
-        }
-        return cheapestHotel+" Total Rate: $"+min_cost;
-    }
-
-    public int calculateTotalHotelCost(Hotel hotel, LocalDate startDate, LocalDate endDate){
-        int totalCost = 0;
-
-        while(!startDate.isAfter(endDate)){
-            totalCost += hotel.getRegularRates(startDate);
-            startDate.plusDays(1);
-        }
-        return totalCost;
-    }
 }
