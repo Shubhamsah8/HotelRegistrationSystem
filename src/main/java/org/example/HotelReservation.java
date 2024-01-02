459c777b89b4982e606316018c146851a836bb42
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:50:14 +0530

    Update HotelReservation.java

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -3,14 +3,27 @@ package org.example;
 import java.time.LocalDate;
 import java.util.*;
 
-public class HotelReservation{
+/*
+    @description: This class represents a hotel reservation system that manages a list of hotels.
+*/
+public class HotelReservation {
     private List<Hotel> hotels;
 
-    public HotelReservation(){
+    /*
+        @description: Constructor to initialize the hotel reservation system with an empty list of hotels.
+        @parameters: None
+    */
+    public HotelReservation() {
         this.hotels = new ArrayList<>();
     }
 
-    public void addHotel(Hotel hotel){
+    /*
+        @description: Adds a hotel to the list of hotels in the reservation system.
+        @parameters:
+            - hotel: The hotel to be added.
+        @return: None
+    */
+    public void addHotel(Hotel hotel) {
         hotels.add(hotel);
     }
 }
