e773a49f2f5c07e8af52165e0b786613ad54b4d2
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:52:15 +0530

    Update Hotel.java

--- src/main/java/org/example/Hotel.java
+++ src/main/java/org/example/Hotel.java
@@ -2,28 +2,51 @@ package org.example;
 
 import java.time.LocalDate;
 import java.util.*;
-import java.time.DayOfWeek;
 
+/*
+    @description: This class represents a hotel with regular rates for different dates.
+*/
 public class Hotel {
 
     private String name;
     private Map<LocalDate, Integer> regularRates;
 
+    /*
+        @description: Constructor to initialize the hotel with a name and an empty map for regular rates.
+        @parameters:
+            - name: The name of the hotel.
+    */
     public Hotel(String name) {
         this.name = name;
         this.regularRates = new HashMap<>();
     }
 
-
+    /*
+        @description: Adds regular rates for a specific date.
+        @parameters:
+            - date: The date for which the regular rate is added.
+            - rate: The regular rate for the specified date.
+        @return: None
+    */
     public void addRegularRates(LocalDate date, int rate) {
         regularRates.put(date, rate);
     }
 
-
+    /*
+        @description: Retrieves the regular rate for a specific date.
+        @parameters:
+            - date: The date for which the regular rate is requested.
+        @return: The regular rate for the specified date.
+    */
     public int getRegularRates(LocalDate date) {
         return regularRates.get(date);
     }
 
+    /*
+        @description: Retrieves the name of the hotel.
+        @parameters: None
+        @return: The name of the hotel.
+    */
     public String getName() {
         return name;
     }
